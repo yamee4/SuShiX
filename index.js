53ad62bb241e03e4020ae3a1590e9e4a5fcac2b8
HuyHangHai <huynoi137@gmail.com>
2024-12-09 23:22:23 +0700

    Update Branch

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-const express = require('express');
-const expressHbs = require('express-handlebars');
+const express = require("express");
+const expressHbs = require("express-handlebars");
 const port = 3000;
 
 const app = express();
 
-app.use('/node_modules', express.static('node_modules'));
+app.use("/node_modules", express.static("node_modules"));
 app.use(express.static(__dirname + "/Assets"));
-
 
 app.engine(
     "hbs",
     expressHbs.engine({
         layoutsDir: __dirname + "/views/layouts",
-        partialsDir:  __dirname + "/views/partials",
+        partialsDir: __dirname + "/views/partials",
         extname: "hbs",
-        defaultLayout: "layout"
+        defaultLayout: "layout",
     })
 );
 
 app.set("view engine", "hbs");
 
+app.get("/", (req, res) => res.redirect("/home"));
 
+app.use("/home", require("./routes/homeRouter"));
+app.use("/menu", require("./routes/menuRouter"));
 
-app.get('/', (req, res) => res.redirect('/home'));
+app.use("/signin", require("./routes/signinRouter"));
+app.use("/signup", require("./routes/signupRouter"));
 
-app.use('/home', require('./routes/homeRouter'));
-app.use('/menu', require('./routes/menuRouter'));
-
-app.use('/signin', require('./routes/signinRouter'));
-app.use('/signup', require('./routes/signupRouter'));
-
-app.listen(port, () => {    
-  console.log('Server is running on port 3000');
+app.listen(port, () => {
+    console.log("Server is running on port 3000");
 });
