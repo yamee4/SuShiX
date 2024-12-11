6b0428de136b436e84ce88919aac237d6d7e9976
Dinhzitduck <nq2019.dinhvietduc140904@gmail.com>
2024-12-11 18:55:54 +0700

    Update Branch,_Phone_Number, Menu, Area data

--- Database/data.sql
+++ Database/data.sql
@@ -1,24 +1,86 @@
 use Nhom3_ADB
 go
 
+INSERT INTO MENU (MenuID, MenuName)
+VALUES
+('M001', 'Classic Menu'),
+('M002', 'Spring Menu'),
+('M003', 'Summer Menu'),
+('M004', 'Autumn Menu'),
+('M005', 'Winter Menu'),
+('M006', 'Vegetarian Menu'),
+('M007', 'Holiday Menu');
+
+INSERT INTO AREA (AreaName, MenuID)
+VALUES
+('Area 1', 'M001'),
+('Area 1', 'M002'),
+('Area 1', 'M003'),
+('Area 1', 'M005'),
+('Area 1', 'M006'),
+('Area 1', 'M007'),
+('Area 2', 'M001'),
+('Area 2', 'M003'),
+('Area 2', 'M004'),
+('Area 2', 'M005'),
+('Area 2', 'M006'),
+('Area 2', 'M007'),
+('Area 3', 'M001'),
+('Area 3', 'M002'),
+('Area 3', 'M003'),
+('Area 3', 'M004'),
+('Area 3', 'M005'),
+('Area 3', 'M007');
+
 INSERT INTO BRANCH (BranchID, BranchName, BranchAddress, OpeningTime, ClosedTime, isParkinglot, AreaName, DeliveryService) VALUES
-(1, N'Sushi Bình Thạnh', N'27A, đường Bạch Đằng, Quận Bình Thạnh, TP.HCM', N'8:00', N'17:30', 0, null, 1),
-(2, N'Sushi Quận 12', N'355, đường Tô Ký, Quận 12, TP.HCM', N'7:45', N'17:30', 1, null, 1),
-(3, N'Sushi Ánh Sao', N'15, đường 15, Quận 7, TP.HCM', N'15:45', N'21:30', 1, null, 1),
-(4, N'Sushi Quận 3', N'27A, đường Nguyễn Thị Minh Khai, Quận 3, TP.HCM', N'9:00', N'18:30', 0, null, 1),
-(5, N'Sushi Quận 4', N'355C, đường Bến Vân Đồn, Quận 4, TP.HCM', N'7:45', N'17:30', 0, null, 1),
-(6, N'Sushi Quận 5', N'167A, đường Nguyễn Văn Cừ, Quận 5, TP.HCM', N'8:45', N'17:45', 1, null, 1),
-(7, N'Sushi Cầu Giấy', N'27A, đường Dịch Vọng, Quận Cầu Giấy, TP.Hà Nội', N'10:00', N'20:30', 1, null, 1),
-(8, N'Sushi Gò Vấp', N'1A, đường Quang Trung, Quận Gò Vấp, TP.HCM', N'7:45', N'17:30', 1, null, 0),
-(9, N'Sushi Bến Thành', N'510, đường Phan Bội Châu, Quận 1, TP.HCM', N'9:45', N'17:30', 1, null, 0),
-(10, N'Sushi Gò Vấp 2', N'2B, đường Nguyễn Huệ, Quận Gò Vấp, TP.HCM', N'10:45', N'17:30', 1, null, 1),
-(11, N'Sushi Tân Sơn Nhất', N'Sân bay Tân Sơn Nhất, TP.HCM', N'5:45', N'17:30', 1, null, 0),
-(12, N'Sushi Nha Trang', N'55, Lê Thành Phương, Vạn Thắng, TP.Nha Trang', N'12:45', N'17:30', 1, null, 1),
-(13, N'Sushi Mạc Đĩnh Chi', N'19 – 21 – 23, Mạc Đĩnh Chi, Đa Kao, Quận 1, TP.HCM', N'13:45', N'16:30', 1, null, 0),
-(14, N'Sushi Phú Lâm', N'	1039 Hồng Bàng, Phường 12, TP.HCM', N'16:30', N'17:30', 1, null, 1),
-(15, N'Sushi Him Lam', N'121 – 123 Nguyễn Thị Thập, Khu Đô Thị Him Lam, Phường Tân Hưng, TP.HCM', N'9:15', N'18:30', 1, null, 0)
+(1, N'Sushi Bình Thạnh', N'27A, đường Bạch Đằng, Quận Bình Thạnh, TP.HCM', N'8:00', N'17:30', 0, 'Area 1', 1),
+(2, N'Sushi Quận 12', N'355, đường Tô Ký, Quận 12, TP.HCM', N'7:45', N'17:30', 1, 'Area 2', 1),
+(3, N'Sushi Ánh Sao', N'15, đường 15, Quận 7, TP.HCM', N'15:45', N'21:30', 1, 'Area 1', 1),
+(4, N'Sushi Quận 3', N'27A, đường Nguyễn Thị Minh Khai, Quận 3, TP.HCM', N'9:00', N'18:30', 0, 'Area 3', 1),
+(5, N'Sushi Quận 4', N'355C, đường Bến Vân Đồn, Quận 4, TP.HCM', N'7:45', N'17:30', 0, 'Area 2', 1),
+(6, N'Sushi Quận 5', N'167A, đường Nguyễn Văn Cừ, Quận 5, TP.HCM', N'8:45', N'17:45', 1, 'Area 1', 1),
+(7, N'Sushi Cầu Giấy', N'27A, đường Dịch Vọng, Quận Cầu Giấy, TP.Hà Nội', N'10:00', N'20:30', 1, 'Area 3', 1),
+(8, N'Sushi Gò Vấp', N'1A, đường Quang Trung, Quận Gò Vấp, TP.HCM', N'7:45', N'17:30', 1, 'Area 2', 0),
+(9, N'Sushi Bến Thành', N'510, đường Phan Bội Châu, Quận 1, TP.HCM', N'9:45', N'17:30', 1, 'Area 1', 0),
+(10, N'Sushi Gò Vấp 2', N'2B, đường Nguyễn Huệ, Quận Gò Vấp, TP.HCM', N'10:45', N'17:30', 1, 'Area 1', 1),
+(11, N'Sushi Tân Sơn Nhất', N'Sân bay Tân Sơn Nhất, TP.HCM', N'5:45', N'17:30', 1, 'Area 3', 0),
+(12, N'Sushi Nha Trang', N'55, Lê Thành Phương, Vạn Thắng, TP.Nha Trang', N'12:45', N'17:30', 1, 'Area 2', 1),
+(13, N'Sushi Mạc Đĩnh Chi', N'19 – 21 – 23, Mạc Đĩnh Chi, Đa Kao, Quận 1, TP.HCM', N'13:45', N'16:30', 1, 'Area 2', 0),
+(14, N'Sushi Phú Lâm', N'	1039 Hồng Bàng, Phường 12, TP.HCM', N'16:30', N'17:30', 1, 'Area 3', 1),
+(15, N'Sushi Him Lam', N'121 – 123 Nguyễn Thị Thập, Khu Đô Thị Him Lam, Phường Tân Hưng, TP.HCM', N'9:15', N'18:30', 1, 'Area 3', 0)
 
-
+INSERT INTO BRANCH_PHONE_NUMBER (BranchID, PhoneNumber)
+VALUES
+(1, '0912345678'),
+(1, '0987654321'),
+(2, '0934567890'),
+(2, '0901234567'),
+(3, '0967890123'),
+(3, '0998765432'),
+(4, '0978901234'),
+(4, '0956789012'),
+(5, '0943210987'),
+(5, '0921098765'),
+(6, '0987654321'),
+(6, '0912345678'),
+(7, '0934567890'),
+(7, '0901234567'),
+(8, '0967890123'),
+(8, '0998765432'),
+(9, '0978901234'),
+(9, '0956789012'),
+(10, '0943210987'),
+(10, '0921098765'),
+(11, '0987654321'),
+(11, '0912345678'),
+(12, '0934567890'),
+(12, '0901234567'),
+(13, '0967890123'),
+(13, '0998765432'),
+(14, '0978901234'),
+(14, '0956789012'),
+(15, '0943210987'),
+(15, '0921098765');
 
 INSERT INTO DISH (DishID, DishName, CurrentPrice, DishSection, isCombo, DeliveryAvailable)
 VALUES
