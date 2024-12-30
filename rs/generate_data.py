ddf96eddb9f27288ebc2ca12b2b8f71a88f72d6d
yamee4 <yamee4804@gmail.com>
2024-12-30 13:37:13 +0700

    test mock_data generator

--- rs/generate_data.py
+++ rs/generate_data.py
@@ -3,10 +3,29 @@ import csv
 from datetime import datetime, timedelta
 
 # Helper functions
+def mapping_price(dishid):
+    prices = [50000, 60000, 45000, 55000, 65000, 60000, 70000, 75000, 80000, 85000, 100000, 30000, 35000, 40000, 50000, 20000, 30000, 40000, 15000, 40000, 35000, 150000, 200000, 250000, 180000, 25000, 30000, 35000, 50000, 30000, 15000, 40000, 35000, 50000, 60000, 55000, 40000, 65000, 80000, 150000, 120000]
+    return prices[dishid - 1]
+
 def random_date(start, end):
     delta = end - start
     random_days = random.randint(0, delta.days)
     return start + timedelta(days=random_days)
+
+
+def generate_employee(num_employees):
+    employees = []
+    for i in range(num_employees):
+        empID = f"{i+1:05d}"
+        first_name = f"FirstName{i+1}"
+        last_name = f"LastName{i+1}"
+        birth_date = random_date(datetime(1990, 1, 1), datetime(2003, 12, 31))
+        gender = random.choice(["Nam", "Nu"])
+        salary = random.randint(5000000, 20000000)
+        branch_manager = 0
+        employees.append([empID, first_name, last_name, birth_date, gender, salary, branch_manager])
+    return employees
+
 
 def generate_customers(num_customers):
     customers = []
@@ -16,7 +35,7 @@ def generate_customers(num_customers):
         last_name = f"LastName{i+1}"
         phone = f"{random.randint(1000000000, 9999999999)}"
         email = f"customer{i+1}@example.com"
-        gender = random.choice(["M", "F"])
+        gender = random.choice(["Nam", "Nu"])
         is_member = random.choice([0, 1])
         is_registered = random.choice([0, 1])
         customers.append([cccd, first_name, last_name, phone, email, gender, is_member, is_registered])
@@ -24,52 +43,52 @@ def generate_customers(num_customers):
 
 def generate_online_customers(customers):
     online_customers = []
-    selected_customers = random.sample(customers, 40000)
-    for customer in selected_customers:
-        password = f"pass{customer[0]}"
-        online_customers.append([customer[0], password])
+    for customer in customers:
+        if customer[7] == 1:
+            password = f"pass{customer[0]}"
+            online_customers.append([customer[0], password])
     return online_customers
 
 def generate_customer_members(customers):
     customer_members = []
-    selected_customers = random.sample(customers, 60000)
-    for customer in selected_customers:
-        member_card = f"MC{random.randint(10000, 99999)}"
-        created_date = random_date(datetime(2015, 1, 1), datetime(2024, 1, 1))
-        support_emp = f"E{random.randint(1, 100):05d}"
-        rank = random.choice(["Bronze", "Silver", "Gold", "Platinum"])
-        points = random.randint(0, 10000)
-        acquired_rank_date = created_date + timedelta(days=random.randint(30, 365))
-        customer_members.append([customer[0], member_card, created_date, support_emp, rank, points, acquired_rank_date])
+    for customer in customers:
+        if customer[6] == 1:
+            member_card = f"MC{random.randint(10000, 99999)}"
+            created_date = random_date(datetime(2015, 1, 1), datetime(2024, 1, 1))
+            support_emp = f"E{random.randint(1, 100):05d}"
+            rank = random.choice(["MEMBER", "SILVER", "GOLD"])
+            points = random.randint(0, 10000)
+            acquired_rank_date = created_date + timedelta(days=random.randint(30, 365))
+            customer_members.append([customer[0], member_card, created_date, support_emp, rank, points, acquired_rank_date])
     return customer_members
 
-def generate_order_tickets(customers):
+def generate_order_tickets(customers, emp):
     order_tickets = []
     ticket_types = ["ONL", "PRE", "STD"]
     for customer in customers:
         ticket_id = f"TKT{random.randint(10000, 99999):07d}"
         ticket_type = random.choice(ticket_types)
         branch_id = random.randint(1, 50)
-        emp_id = f"E{random.randint(1, 100):05d}"
-        discount = random.randint(0, 50)
-        total_price = random.randint(100, 10000)
-        created_date = random_date(datetime(2015, 1, 1), datetime(2024, 1, 1))
+        emp_id = random.choice(emp)
+        discount = 0
+        total_price = 0
+        created_date = random_date(datetime(2020, 1, 1), datetime(2024, 1, 1))
         order_tickets.append([ticket_id, ticket_type, branch_id, customer[0], emp_id, discount, total_price, created_date])
     return order_tickets
 
-def generate_standard_order_tickets(order_tickets):
+def generate_standard_order_tickets(order_tickets, emp):
     standard_tickets = []
     for ticket in filter(lambda x: x[1] == "STD", order_tickets):
         table_name = f"Table{random.randint(1, 100)}"
-        support_employee = f"E{random.randint(1, 100):05d}"
+        support_employee = random.choice(emp)
         standard_tickets.append([ticket[0], table_name, support_employee])
     return standard_tickets
 
 def generate_pre_order_tickets(order_tickets):
     pre_tickets = []
     for ticket in filter(lambda x: x[1] == "PRE", order_tickets):
-        branch_name = f"Branch{random.randint(1, 50)}"
-        area = f"Area{random.randint(1, 20)}"
+        branch_name = {random.randint(1, 15)}
+        area = f"Area {random.randint(1, 4)}"
         num_customers = random.randint(1, 10)
         pre_order_note = f"Note for {ticket[0]}"
         pre_tickets.append([ticket[0], branch_name, area, num_customers, pre_order_note])
@@ -84,13 +103,11 @@ def generate_online_tickets(order_tickets):
 def generate_ticket_details(order_tickets, ticket_type):
     ticket_details = []
     for ticket in filter(lambda x: x[1] == ticket_type, order_tickets):
-        num_dishes = random.randint(1, 5)
-        for _ in range(num_dishes):
-            dish_id = random.randint(1, 40)
-            order_time = random_date(datetime(2023, 1, 1), datetime(2024, 1, 1))
-            quantity = random.randint(1, 5)
-            price = random.randint(50, 500)
-            ticket_details.append([ticket[0], dish_id, order_time, quantity, price])
+        dish_id = random.randint(1, 40)
+        order_time = random_date(datetime(2023, 1, 1), datetime(2024, 12, 31))
+        quantity = random.randint(1, 5)
+        price = quantity * mapping_price(dish_id)
+        ticket_details.append([ticket[0], dish_id, order_time, quantity, price])
     return ticket_details
 
 def save_to_csv(filename, data, headers):
@@ -100,11 +117,12 @@ def save_to_csv(filename, data, headers):
         writer.writerows(data)
 
 # Generate data
+employees = generate_employee(5000)
 customers = generate_customers(100000)
 online_customers = generate_online_customers(customers)
 customer_members = generate_customer_members(customers)
-order_tickets = generate_order_tickets(customers)
-standard_tickets = generate_standard_order_tickets(order_tickets)
+order_tickets = generate_order_tickets(customers, employees)
+standard_tickets = generate_standard_order_tickets(order_tickets, employees)
 pre_tickets = generate_pre_order_tickets(order_tickets)
 online_tickets = generate_online_tickets(order_tickets)
 online_ticket_details = generate_ticket_details(order_tickets, "ONL")
